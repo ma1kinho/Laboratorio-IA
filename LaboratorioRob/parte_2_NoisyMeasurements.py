6fb2f428f403b6d963ddb979151697c3e0dfbbfd
William Gabriel da Paz Rosendo <52809519+ma1kinho@users.noreply.github.com>
2023-10-24 00:19:39 -0300

    Add files via upload

---
+++ LaboratorioRob/parte_2_NoisyMeasurements.py
@@ -0,0 +1,102 @@
+from math import pi, sqrt
+from matrix import matrix # Check the matrix.py tab to see how this works.
+from robot import robot  # Check the robot.py tab to see how this works.
+
+from CircularMotionKalmanFilter import CircularMotionKalmanFilter
+
+def estimate_next_pos(measurement, OTHER=None):
+    """Estimate the next (x, y) position of the wandering Traxbot
+    based on noisy (x, y) measurements.
+
+    I use an Extended Kalman Filter (EKF) to estimate five parameters:
+        1. the x position
+        2. the y position
+        3. the current heading direction of the robot
+        4. the angle the robot turns between measurements
+        5. the distance the robot travels between measurements
+    """
+
+    # create an instance of the EKF
+    if not OTHER:
+        OTHER = CircularMotionKalmanFilter()
+
+    # set up the measurement, updates the EKF and predict the next step
+    z = matrix([
+        [measurement[0]],
+        [measurement[1]]
+    ])
+    OTHER.updateMeasurement(z)
+    x, y = OTHER.predict()
+
+    return (x, y), OTHER
+
+# A helper function you may find useful.
+def distance_between(point1, point2):
+    """Computes distance between point1 and point2. Points are (x, y) pairs."""
+    x1, y1 = point1
+    x2, y2 = point2
+    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+def demo_grading(estimate_next_pos_fcn, target_bot, OTHER=None):
+    """
+    This is here to give you a sense for how we will be running and grading
+    your code. Note that the OTHER variable allows you to store any
+    information that you want.
+    """
+    localized = False
+    distance_tolerance = 0.01 * target_bot.distance
+    ctr = 0
+    # if you haven't localized the target bot, make a guess about the next
+    # position, then we move the bot and compare your guess to the true
+    # next position. When you are close enough, we stop checking.
+    while not localized and ctr <= 1000:
+        ctr += 1
+        measurement = target_bot.sense()
+        position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
+        target_bot.move_in_circle()
+        true_position = (target_bot.x, target_bot.y)
+        error = distance_between(position_guess, true_position)
+        if error <= distance_tolerance:
+            # print "You got it right! It took you ", ctr, " steps to localize."
+            localized = True
+        if ctr == 1000:
+            # print "Sorry, it took you too many steps to localize the target."
+            return localized, ctr
+    return localized, ctr
+
+# This is a demo for what a strategy could look like. This one isn't very good.
+def naive_next_pos(measurement, OTHER=None):
+    """This strategy records the first reported position of the target and
+    assumes that eventually the target bot will eventually return to that
+    position, so it always guesses that the first position will be the next."""
+    if not OTHER: # this is the first measurement
+        OTHER = measurement
+    xy_estimate = OTHER
+    return xy_estimate, OTHER
+
+# run multiple trials of the program to get a good sense of its capability
+NUM_TRIALS = 5
+nLocalized = 0
+counter = []
+for i in range(NUM_TRIALS):
+    # create a test target
+    test_target = robot(2.1, 4.3, 0.5, 2*pi / 34.0, 1.5)
+    measurement_noise = 0.05 * test_target.distance
+    test_target.set_noise(0.0, 0.0, measurement_noise)
+    # run the estimation function to decided if the target is found
+    found, it = demo_grading(estimate_next_pos, test_target)
+    # record the successes!
+    if found:
+        nLocalized += 1
+        counter.append(it)
+
+if nLocalized > 0:
+    # print out the final results with success rate and the average iteration number
+    successPercent = 100.0 * nLocalized / NUM_TRIALS
+    avgCounter = float(sum(counter)) / len(counter)
+    print('Localized {:.2f}% of the {:d} trials'.format(successPercent, NUM_TRIALS))
+    print('Averaged {:.1f} iterations'.format(avgCounter))
+
+else:
+    print('All {:d} attempt(s) failed!!'.format(NUM_TRIALS))
---
+++ LaboratorioRob/parte_3_Chase.py
@@ -0,0 +1,148 @@
+
+from math import atan2, pi, sqrt
+from matrix import matrix
+from robot import robot
+
+from CircularMotionKalmanFilter import CircularMotionKalmanFilter
+
+def angle_trunc(a):
+    """
+    helper function to map all angles onto [-pi, pi]
+    """
+    while a < 0.0:
+        a += pi * 2
+    return ((a + pi) % (pi * 2)) - pi
+
+
+def next_move(hunter_position, hunter_heading, target_measurement, max_distance, OTHER=None):
+    """
+    This function is called after every measurement to move the hunter robot.
+    """
+    # create an instance of the EKF
+    if not OTHER:
+        OTHER = CircularMotionKalmanFilter()
+
+    # set up the measurement, updates the EKF and predict the next step
+    z = matrix([
+        [target_measurement[0]],
+        [target_measurement[1]]
+    ])
+    OTHER.updateMeasurement(z)
+    predict_target_position = OTHER.predict()
+
+    heading_to_target = get_heading(hunter_position, predict_target_position)
+    heading_difference = heading_to_target - hunter_heading
+    turning = heading_difference
+    # move as much as the max_distance or the distance to the target
+    distance = min(max_distance, distance_between(hunter_position, predict_target_position))
+    return turning, distance, OTHER
+
+def distance_between(point1, point2):
+    """Computes distance between point1 and point2. Points are (x, y) pairs."""
+    x1, y1 = point1
+    x2, y2 = point2
+    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+def demo_grading(hunter_bot, target_bot, next_move_fcn, OTHER=None):
+    """Returns True if your next_move_fcn successfully guides the hunter_bot
+    to the target_bot. This function is here to help you understand how we
+    will grade your submission."""
+    max_distance = 1.94 * target_bot.distance # 1.94 is an example. It will change.
+    # hunter must be within 0.02 step size to catch target
+    separation_tolerance = 0.02 * target_bot.distance
+    caught = False
+    ctr = 0
+
+    # We will use your next_move_fcn until we catch the target or time expires.
+    while not caught and ctr < 1000:
+
+        # Check to see if the hunter has caught the target.
+        hunter_position = (hunter_bot.x, hunter_bot.y)
+        target_position = (target_bot.x, target_bot.y)
+        separation = distance_between(hunter_position, target_position)
+        if separation < separation_tolerance:
+            # print "You got it right! It took you ", ctr, " steps to catch the target."
+            caught = True
+
+        # The target broadcasts its noisy measurement
+        target_measurement = target_bot.sense()
+
+        # This is where YOUR function will be called.
+        turning, distance, OTHER = next_move_fcn(
+            hunter_position,
+            hunter_bot.heading,
+            target_measurement,
+            max_distance, OTHER
+        )
+
+        # Don't try to move faster than allowed!
+        if distance > max_distance:
+            distance = max_distance
+
+        # We move the hunter according to your instructions
+        hunter_bot.move(turning, distance)
+
+        # The target continues its (nearly) circular motion.
+        target_bot.move_in_circle()
+
+        ctr += 1
+        if ctr >= 1000:
+            # print "It took too many steps to catch the target."
+            return caught, ctr
+    return caught, ctr
+
+def get_heading(hunter_position, target_position):
+    """Returns the angle, in radians, between the target and hunter positions"""
+    hunter_x, hunter_y = hunter_position
+    target_x, target_y = target_position
+    heading = atan2(target_y - hunter_y, target_x - hunter_x)
+    heading = angle_trunc(heading)
+    return heading
+
+def naive_next_move(hunter_position, hunter_heading, target_measurement, max_distance, OTHER):
+    """This strategy always tries to steer the hunter directly towards where the target last
+    said it was and then moves forwards at full speed. This strategy also keeps track of all
+    the target measurements, hunter positions, and hunter headings over time, but it doesn't
+    do anything with that information."""
+    if not OTHER: # first time calling this function, set up my OTHER variables.
+        measurements = [target_measurement]
+        hunter_positions = [hunter_position]
+        hunter_headings = [hunter_heading]
+        OTHER = (measurements, hunter_positions, hunter_headings) # now I can keep track of history
+    else: # not the first time, update my history
+        OTHER[0].append(target_measurement)
+        OTHER[1].append(hunter_position)
+        OTHER[2].append(hunter_heading)
+        measurements, hunter_positions, hunter_headings = OTHER
+
+    heading_to_target = get_heading(hunter_position, target_measurement)
+    heading_difference = heading_to_target - hunter_heading
+    turning = heading_difference # turn towards the target
+    distance = max_distance # full speed ahead!
+    return turning, distance, OTHER
+
+# run multiple trials of the program to get a good sense of its capability
+NUM_TRIALS = 100
+nCaptured = 0
+counter = []
+for i in range(NUM_TRIALS):
+    # create a test target
+    target = robot(0.0, 10.0, 0.0, 2*pi / 30, 1.5)
+    measurement_noise = .05*target.distance
+    target.set_noise(0.0, 0.0, measurement_noise)
+    # run the estimation function to decided if the target is captured
+    hunter = robot(-10.0, -10.0, 0.0)
+    found, it = demo_grading(hunter, target, next_move)
+    # record the successes!
+    if found:
+        nCaptured += 1
+        counter.append(it)
+
+if nCaptured > 0:
+    # print out the final results with success rate and the average iteration number
+    successPercent = 100.0 * nCaptured / NUM_TRIALS
+    avgCounter = float(sum(counter)) / len(counter)
+    print ('Captured {:.2f}% of the {:d} trials'.format(successPercent, NUM_TRIALS))
+    print ('Averaged {:.1f} iterations'.format(avgCounter))
+else:
+    print ('All {:d} attempt(s) failed!!'.format(NUM_TRIALS))
